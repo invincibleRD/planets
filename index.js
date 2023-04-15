d154523c212e8f3c724d52bacfb8a991a7152769
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2023-04-15 12:17:29 +0530

    filtered habitable planets

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-const parse =require('csv-parse');
-const fs =require('fs');
+const { log } = require("console");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+function habitable(planet) {
+  return (
+    planet.koi_disposition === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (chunks) => {
+    if (habitable(chunks)) {
+      habitablePlanets.push(chunks);
+    }
+  })
+  .on("end", () => {
+    log(habitablePlanets.map(planet=>{
+       return planet["kepler_name"]
+    }));
+    log(`Done 🔥 Total habitable planets: ${habitablePlanets.length}`);
+  })
+  .on("error", (error) => {
+    log("Error", error);
+  });
